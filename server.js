a227f9291681eb0c2dfb51a890e3720daf24f01b
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-15 17:29:11 +0100

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './route/route';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
+
+app.listen(process.env.PORT_TEST, () => {
+    console.log('server now running at ' + process.env.PORT_TEST);
+});
+
+// load the routes
+export default app;
