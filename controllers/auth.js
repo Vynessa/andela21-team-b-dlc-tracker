c5ba9e093c608c2cfbb713885f3e75b87496d747
victor4l <victoridongesit@gmail.com>
2017-05-15 20:53:20 +0100

    feat: Auth and Routes Created

---
+++ controllers/auth.js
@@ -0,0 +1,65 @@
+import firebase from '../helpers/firebase';
+
+const db = firebase.database();
+const ref = db.ref();
+const fireBase = firebase.auth();
+
+// register user
+module.exports.register = (req, res) => {
+  const firstName = req.body.firstName,
+    lastName = req.body.lastName,
+    email = req.body.email,
+    password = req.body.password,
+    state = req.body.state,
+    country = req.body.country,
+    phone = req.body.phone;
+
+  fireBase.createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      const userId = user.uid;
+      const userRef = ref.child(`users/${userId}`);
+      return userRef.set({
+        userId,
+        firstName,
+        lastName,
+        phone,
+        state,
+        country,
+        email,
+        password
+      });
+    })
+    .then(res.redirect('/dashboard'))
+    .catch((err) => {
+      res.redirect('/');
+    });
+};
+
+
+// signin with email and password
+module.exports.login = (req, res) => {
+  const email = req.body.email,
+    password = req.body.password;
+
+  fireBase.signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      const firstName = user.firstName;
+      res.redirect('/dashboard');
+    })
+    .catch((err) => {
+      const errorMessage = err.message;
+      return res.render('index', { error: errorMessage });
+    });
+};
+
+// signOut
+module.exports.signOut = (req, res) => {
+  fireBase.signOut()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      const errorMessage = err.message;
+      return res.render('index', { error: errorMessage });
+    });
+};
---
+++ helpers/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
+
+export default firebase.initializeApp(config);
