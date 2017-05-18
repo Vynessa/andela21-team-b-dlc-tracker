86a808c773b584733dae28d44ccc9b6f23948618
tomi <tomipaul95@gmail.com>
2017-05-18 10:44:51 +0100

    [refactor]: change response type to json

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
           communityDescriptions.push(community.description);
         }
       }
-      res.render('dashboard', { userData, userCommunities, communities: communityDescriptions });
+      res.json(userData);
     });
   }
 });
