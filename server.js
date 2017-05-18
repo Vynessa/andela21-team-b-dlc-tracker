e618c4ffecae2a7f5c00e20252c5fd2dd097652f
benjamintaiwo <abegundetaiwo@gmail.com>
2017-05-18 16:35:34 +0100

    fix: edit route

--- server.js
+++ server.js
@@ -1,43 +1,64 @@
-// import firebase from 'firebase';
-import express from 'express';
-import config from './firebaseConfig.json';
 import dotenv from 'dotenv';
+import express from 'express';
 import bodyParser from 'body-parser';
-import adminrouter from './routes/admin';
+import auth from './controllers/auth';
+import modules from './routes/modules';
+import dashboard from './routes/dashboard';
+import firebase from './helpers/firebase';
 
 dotenv.config();
+const db = firebase.database();
+
 const app = express();
-const db = database.firebase();
+
+// app.set('views', process.cwd() + '/views');
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 
-switch (process.env.NODE_ENV) {
-  case 'TEST': app.set('PORT', process.env.PORT_TEST); break;
-  case 'PROD': app.set('PORT', process.env.PORT_PROD); break;
-  case 'DEV': app.set('PORT', process.env.PORT_DEV); break;
-  default: app.set('PORT', 5000);
-}
-
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Append user details to the request object
 app.use((req, res, next) => {
   const user = firebase.auth().currentUser;
   if (user) {
-    db.ref(`users/${user.uid}`).on('value', (snapshot) => {
+    db.ref(`users/${user.uid}`).once('value', (snapshot) => {
       req.user = snapshot.val();
       next();
     });
   } else {
-    res.status(401).redirect('/');
+    next();
   }
 });
-app.use('/', adminrouter);
 
-const port = app.get('PORT');
-const server = app.listen(process.env.PORT || port, () => {
-  console.log('server now running at ' + process.env.PORT_TEST);
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
+const port = process.env.PORT || 5050;
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
 
-// load the routes
-export default server;
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+app.post('/register', auth.register);
+app.post('/login', auth.login);
+app.get('/signOut', auth.signOut);
+app.get('/modules', modules);
+app.get('/modules/:comm', modules);
+app.get('/modules/:comm/:id', modules);
+app.get('/dashboard', dashboard);
+export default app;
