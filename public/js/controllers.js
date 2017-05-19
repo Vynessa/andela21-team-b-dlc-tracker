6a38d2d2962ec613616aecdfaee3d15fa5e8d80c
tomi <tomipaul95@gmail.com>
2017-05-19 02:43:54 +0100

    [feat]: Add angular controllers

---
+++ public/js/controllers.js
@@ -0,0 +1,37 @@
+angular.module('dlctrackerApp', [])
+.controller('appCtrl', ($scope, dlctrackerService) => {
+  $scope.testIsShown = false;
+  $scope.videoSrc = '';
+  $scope.videoIsShown = false;
+  $scope.showVideo = () => {
+    $scope.videoSrc = $scope.module.video;
+    $scope.videoIsShown = true;
+  };
+  $scope.showTest = () => {
+    $scope.testIsShown = true;
+  };
+  $scope.getModule = () => {
+    dlctrackerService.getModule(modId, (resData) => {
+      $scope.module = resData;
+    });
+  };
+
+  $scope.hasCommunity = (commName) => {
+    if (Object.prototype.hasOwnProperty($scope.user, 'communities')) {
+      return Object.prototype.hasOwnProperty.call($scope.user.communities, commName);
+    }
+    return false;
+  };
+  
+  dlctrackerService.getUser((resData) => {
+    $scope.user = resData;
+  });
+  dlctrackerService.getCommunities((resData) => {
+    $scope.communities = resData;
+  });
+  dlctrackerService.getUsers((resData) => {
+    console.log(resData);
+    $scope.userLength = Object.keys(resData).length;
+  });
+});
+
